4026a18fe7c221d4c315a9cbb74b7d0a3c2d8a10
updaun <updauney@daum.net>
2023-12-15 23:51:10 +0900

    [feat] design update

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,31 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+console.log(section);
+let navLinks = document.querySelectorAll('header .navbar a');
+console.log(navLinks);
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if (top => offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+        }
+
+    });
+
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -33,4 +55,33 @@ var swiper = new Swiper(".home-slider", {
         clickable: true,
     },
     loop:true,
+});
+
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
